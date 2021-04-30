a4ccfc47ac01bec7c27317476f48233a92460ce2
marigut <mari.gutierrez96@yahoo.com>
2021-04-30 17:17:15 -0500

    search movies logic attempt

--- assests/script.js
+++ assests/script.js
@@ -3,30 +3,19 @@ var searchInputEl = document.querySelector("#search");
 var multiContainerEl = document.querySelector("#multi-container");
 var multiSearchTerm = document.querySelector("#multi-search-term");
 
-var id = 550;
+var moviesPath = "data.results[0].(id/media_type/title/poster_path/release_date)";
+var tvPath = "data.results[i].(id/media_type/name/poster_path/first_air_date)";
+var actorsPath = "data.results[i].(id/media_type/name/profile_path)";
 mediaType = "movie" + "tv" + "person";
 
-var getMultiSearch = function() {
+var getMultiSearch = function(searchRequest) {
   // format the multi search api url
-  var apiUrl = "https://api.themoviedb.org/3/search/multi" + mediaType + "/" + id + "?api_key=b7854a2f58fc72f2408614bd5147ec1c&language=en-US&page=1&query=";
-
+  var apiUrl = "https://api.themoviedb.org/3/search/multi?api_key=b7854a2f58fc72f2408614bd5147ec1c&language=en-US&page=1&query=" + searchRequest;
   // make a request to the url
   fetch(apiUrl).then(function(response) {
     if (response.ok) {
       response.json().then(function(data) {
-        displaySearch(data);
-        var searchDetails = document.createElement("div");
-        var name = "";
-        // Logic to display appropriate information based on mediatype of movie or TV
-        if (mediaType === "movie") {
-          name = data.title;
-        } else {
-          name = data.name;
-        }
-        searchDetails.innerHTML =
-        "<h2 class='title'>" + name + "</h2>";
-        // append container to the dom
-        multiContainerEl.appendChild(searchDetails);
+        displaySearch(data, searchRequest);
       });
       } else {
         alert("Error: Search Not Found");
@@ -34,7 +23,59 @@ var getMultiSearch = function() {
     });
 };
 
+var searchHandler = function(event){
+  event.preventDefault();
+  var searchRequest = searchInputEl.value.trim();
+  if(searchRequest){
+      multiSearchTerm.textContent = searchRequest;
+      getMultiSearch(searchRequest);
+      searchInputEl.value = "";
+  }
+}
+
+var displaySearch = function(multi, searchTerm) {
+ 
+  // check if api returned any multi searches
+  if (multi.length === 0) {
+    multiContainerEl.textContent = "No Searches found.";
+    return;
+  }
+  // clear old content
+  multiContainerEl.textContent = "";
+  multiSearchTerm.textContent = searchTerm;
+ 
+  // loop over repos
+  for (var i = 0; i < multi.length; i++) {
+    
+    // create a container for each search
+    var multiEl = document.createElement("div");
+    multiEl.classList = "list-item flex-row justify-space-between align-center";
+    var name = ""; 
+    // Logic to display appropriate information based on mediatype of movie, TV or actor
+    if (mediaType === "movie") {
+      name = moviePath + data.title[i].profile_path;
+    } else if (mediaType === "tv") {
+      name = tvPath + data.title[i].profile_path;
+    } else {
+      name = moviePath + data.title[i].profile_path;
+    }
+    
+    
+    // create a span element to hold search name
+    var titleEl = document.createElement("span");
+    titleEl.textContent = name;
+ 
+    // append to container
+    multiEl.appendChild(titleEl);
+ 
+    
+    // append container to the dom
+    multiContainerEl.appendChild(multiEl);
+  }
+};
+ 
 
 
+getMultiSearch();
 // add event listeners to forms
-searchBarEl.addEventListener("submit", getMultiSearch);
+searchBarEl.addEventListener("submit", searchHandler);
